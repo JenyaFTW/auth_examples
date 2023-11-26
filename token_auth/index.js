98223ebdefd30f00b9d082398aa83323b556b56f
JenyaFTW <yyakovliev02@gmail.com>
2023-11-26 16:20:45 +0200

    feat: implement auth0

--- token_auth/index.js
+++ token_auth/index.js
@@ -1,68 +1,71 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 const path = require('path');
+const { AuthenticationClient, ManagementClient, UserInfoClient } = require('auth0');
 const port = 3000;
+
+const auth0 = new AuthenticationClient({
+    domain: 'yevgen-kpi.eu.auth0.com',
+    clientId: '7Olt2WPXxlf56SYb41BOyGAfxiiyGAe6',
+    clientSecret: '90P5wXl-xE2IQEcmG9e42Xs2GNpKt9YfAZ5Gq84Ef_Jc2v8Sbi6P4RSL0ZwQdTwQ',
+});
+
+const userInfo = new UserInfoClient({
+    domain: 'yevgen-kpi.eu.auth0.com',
+});
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const TOKEN_KEY = 'Authorization';
-const JWT_SECRET = '5a633d7f34a0b59fe433c01c704625bb';
 
-app.use((req, _, next) => {
+app.use(async (req, _, next) => {
     let user = {};
-    const token = req.get(TOKEN_KEY);
-    if (token) {
-        const decoded = jwt.verify(token, JWT_SECRET);
-        user = decoded;
-    }
-
+    
+    try {
+        const token = req.get(TOKEN_KEY);
+        if (token) {
+            const { data } = await userInfo.getUserInfo(token);
+            user = data;
+        }
+    } catch(err) {}
+    
     req.user = user;
 
     next();
 });
 
 app.get('/', (req, res) => {
-    if (req.user.username) {
-        return res.json({
-            username: req.user.username,
-        });
+    if (req.user.nickname) {
+        return res.json(req.user);
     }
+
     res.sendFile(path.join(__dirname+'/index.html'));
 });
-
-const users = [
-    {
-        login: 'Login',
-        password: 'Password',
-        username: 'Username',
-    },
-    {
-        login: 'Login1',
-        password: 'Password1',
-        username: 'Username1',
-    }
-]
 
 app.post('/api/login', async (req, res) => {
     const { login, password } = req.body;
 
-    const user = users.find((user) => {
-        if (user.login == login && user.password == password) {
-            return true;
-        }
-        return false
-    });
-
-    if (user) {
-        const token = await jwt.sign(user, JWT_SECRET);
-
-        res.json({ token });
+    try {
+        const { data } = await auth0.oauth.passwordGrant({
+            username: login,
+            password,
+            scope: 'offline_access'
+        });
+        
+        res.json(data).send();
+    } catch(err) {
+        res.status(401).send();
     }
+});
 
-    res.status(401).send();
+app.post('/api/refresh', async (req, res) => {
+    const refresh_token = req.get(TOKEN_KEY);
+
+    const { data } = await auth0.oauth.refreshTokenGrant({ refresh_token });
+
+    res.json(data).send();
 });
 
 app.listen(port, () => {

e853046358618bcf98b894f3485b340ac0f0fa99
JenyaFTW <yyakovliev02@gmail.com>
2023-12-21 00:33:44 +0200

    feat: lab5

--- token_auth/index.js
+++ token_auth/index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { AuthenticationClient, ManagementClient, UserInfoClient } = require('auth0');
+const { AuthenticationClient, UserInfoClient } = require('auth0');
+const { auth } = require('express-oauth2-jwt-bearer');
 const port = 3000;
 
 const auth0 = new AuthenticationClient({
     domain: 'yevgen-kpi.eu.auth0.com',
     clientId: '7Olt2WPXxlf56SYb41BOyGAfxiiyGAe6',
     clientSecret: '90P5wXl-xE2IQEcmG9e42Xs2GNpKt9YfAZ5Gq84Ef_Jc2v8Sbi6P4RSL0ZwQdTwQ',
+});
+
+const checkJwt = auth({
+    audience: 'http://localhost:3000/',
+    issuerBaseURL: `https://yevgen-kpi.eu.auth0.com/`,
 });
 
 const userInfo = new UserInfoClient({
@@ -20,28 +26,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const TOKEN_KEY = 'Authorization';
 
-app.use(async (req, _, next) => {
-    let user = {};
-    
-    try {
-        const token = req.get(TOKEN_KEY);
-        if (token) {
-            const { data } = await userInfo.getUserInfo(token);
-            user = data;
-        }
-    } catch(err) {}
-    
-    req.user = user;
-
-    next();
+app.get('/api/me', checkJwt, async (req, res) => {
+    const auth = req.auth;
+    if (auth) {
+        const token = auth.token;
+        const { data } = await userInfo.getUserInfo(token);
+        return res.json(data);
+    }
+    return res.status(401).json({'error': 'Forbidden'});
 });
 
 app.get('/', (req, res) => {
-    if (req.user.nickname) {
-        return res.json(req.user);
-    }
-
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.post('/api/login', async (req, res) => {
@@ -51,11 +47,12 @@ app.post('/api/login', async (req, res) => {
         const { data } = await auth0.oauth.passwordGrant({
             username: login,
             password,
-            scope: 'offline_access'
+            scope: 'offline_access openid',
+            audience: 'http://localhost:3000/'
         });
-        
+
         res.json(data).send();
-    } catch(err) {
+    } catch (err) {
         res.status(401).send();
     }
 });
